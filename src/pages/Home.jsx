28963d4bc82aa9543b4cd2d7d17d0546470bbbf2
Nwose <andreinowse@gmail.com>
2023-06-19 19:19:45 +0100

    added all the files from the blocknode project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import Selection from "../components/Selection";
+
+const Home = () => {
+  return (
+    <div className="relative">
+      <Hero />
+      <Selection />
+    </div>
+  );
+};
+
+export default Home;
