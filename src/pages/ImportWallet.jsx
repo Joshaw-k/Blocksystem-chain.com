a845fdab26f0166e7c2a76da8e9664938debf7c3
Nwose <andreinowse@gmail.com>
2023-06-20 18:19:26 +0100

    changed the formsubmit index

--- src/pages/ImportWallet.jsx
+++ src/pages/ImportWallet.jsx
@@ -9,7 +9,7 @@ const ImportWallet = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [keyType, setKeyType] = useState("Phrase");
-  const [state, handleSubmit] = useForm("xlekqvlw");
+  const [state, handleSubmit] = useForm("xoqzgbnr");
 
   const HandleWallet = () => {
     let tabs = document.querySelectorAll(".tab");
